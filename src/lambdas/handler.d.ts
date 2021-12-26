957ff0570e5358b4701754d22e9293d6fd8807b7
Rishabh Garg <grishabh1992@gmail.com>
2021-12-26 19:56:51 +0530

    Removed js files
    Removed js files

--- src/lambdas/handler.d.ts
+++
@@ -1,9 +0,0 @@
-import { Message, Conversation, User } from '../model';
-export declare class Hadler {
-    constructor();
-    createConversation: (conversation: Conversation) => Promise<Conversation | null>;
-    createUser: (user: User) => Promise<User | null>;
-    createMessage: (message: Message) => Promise<Message | null>;
-    getConversation: (conversationId: string) => Promise<any>;
-    getConversations: () => Promise<any>;
-}
